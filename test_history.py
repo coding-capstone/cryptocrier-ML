09300bb803e7f726ed0f987f69607b6d5ac7f27c
Jonathan Lifferth <jonathan.lifferth@gmail.com>
2022-04-06 15:29:28 -0700

    updated JSON object with json.dumps()

--- test_history.py
+++ test_history.py
@@ -1,7 +1,10 @@
 import requests
+import json
 
-history_request = '{"ticker": "BTC-USD","start":"2021-01-01T00:00:00.000Z","end": "2022-01-01T00:00:00.000Z","interval": "1mo"}'
+history_request = {"ticker": "BTC-USD","start":"2021-01-01T00:00:00.000Z","end": "2022-01-01T00:00:00.000Z","interval": "1mo"}
 
-history_result = requests.post('http://127.0.0.1:5000/history', json=history_request)
+history_str = json.dumps(history_request)
+
+history_result = requests.post('http://127.0.0.1:5000/history', json=history_str)
 if history_result.ok:
     print(history_result.json())
--- test_prediction.py
+++ test_prediction.py
@@ -1,7 +1,10 @@
 import requests
+import json
 
 prediction_request = '{"ticker": "BTC-USD","start":"2021-01-01T00:00:00.000Z","end": "2022-01-01T00:00:00.000Z","interval": "1h", "model": "lr"}'
 
-result = requests.post('http://127.0.0.1:5000/prediction', json=prediction_request)
+prediction_str = json.dumps(prediction_request)
+
+result = requests.post('http://127.0.0.1:5000/prediction', json=prediction_str)
 if result.ok:
     print(result.json())
